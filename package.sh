e500e22752ee411d98d9fad41a36b3d45cb7af0f
Michael Stegeman <michael@stegeman.me>
2020-03-03 13:52:59 -0900

    Update workflows.

--- package.sh
+++ package.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
+#!/bin/bash -e
 
-rm -rf node_modules
+git submodule init
+git submodule update
+
 npm install --production
-rm -rf node_modules/.bin
-rm -f SHA256SUMS
-sha256sum manifest.json package.json *.js README.md > SHA256SUMS
-find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
-TARFILE=$(npm pack)
+shasum --algorithm 256 manifest.json package.json *.js README.md > SHA256SUMS
+find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+
+TARFILE=`npm pack`
+
 tar xzf ${TARFILE}
+
 cp -r node_modules ./package
 mkdir -p package/fliclib-linux-hci/clientlib/nodejs
 cp fliclib-linux-hci/clientlib/nodejs/fliclibNodeJs.js package/fliclib-linux-hci/clientlib/nodejs
@@ -18,10 +21,14 @@ cat "fliclib-linux-hci/LICENSE (for the flicd binary).txt" >> package/LICENSE
 echo "" >> package/LICENSE
 echo "flicd node handler base:" >> package/LICENSE
 cat "fliclib-linux-hci/COPYING (for the documentation and source code).txt" >> package/LICENSE
+
 cd package
-sha256sum LICENSE >> SHA256SUMS
-find fliclib-linux-hci -type f -exec sha256sum {} \; >> SHA256SUMS
+shasum --algorithm 256 LICENSE >> SHA256SUMS
+find fliclib-linux-hci -type f -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd ..
+
 tar czf ${TARFILE} package
-rm -rf package
-echo "Created ${TARFILE}"
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
+rm -rf SHA256SUMS package
